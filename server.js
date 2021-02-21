8fb875bb0d68fa6e0fafd7154b7bf0e1d6a6569b
Ilya Nesterovich <manager@inesterovich.ru>
2021-02-21 09:01:07 +0300

    feat: split routes for different products

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ server.use('/api/secure', require('./routes/secure.routes'));
 const PORT = config.get('port') || 5000;
 
 if (process.env.NODE_ENV === 'production') {
+    
     server.use('/', express.static(path.join(__dirname, 'client', 'build')));
-    server.get('*', (req, res) => { 
+    server.get('fadviser.ru/*', (req, res) => { 
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
+    
 }
 
 
