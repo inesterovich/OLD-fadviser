4e11072ec6e3d27d8d5075d502fcebfdee41b0b6
Ilya Nesterovich <manager@inesterovich.ru>
2021-02-21 10:47:19 +0300

    feat: add different port for nqinx

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = config.get('port') || 5000;
 if (process.env.NODE_ENV === 'production') {
     
     server.use('/', express.static(path.join(__dirname, 'client', 'build')));
-    server.get('fadviser.ru/*', (req, res) => { 
+    server.get('*', (req, res) => { 
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
     
